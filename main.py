bfe2feee1a0a57cfe0818590fc061c21a90caf07
sonlm <son@olli-ai.com>
2024-12-29 11:16:20 +0700

    Let user update MAX_CONCURRENT_TASKS and MAX_CONCURRENT_SITEMAPS

--- main.py
+++ main.py
@@ -10,30 +10,32 @@ from bs4 import BeautifulSoup
 USER_AGENT = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
 }
-MAX_CONCURRENT_TASKS = 1
-MAX_CONCURRENT_SITEMAPS = 5
 
 
 class Site(object):
-    def __init__(self, domain, sitemaps_link, exclude_urls, log_container):
+    def __init__(self, domain, sitemaps_link, exclude_urls, log_queue, max_concurrent_tasks, max_concurrent_sitemaps):
         self.domain = domain
         self.sitemaps_link = sitemaps_link
         self.exclude_urls = exclude_urls
-        self.log_container = log_container
+        self.log_queue = log_queue
+        self.max_concurrent_tasks = max_concurrent_tasks
+        self.max_concurrent_sitemaps = max_concurrent_sitemaps
 
-    async def get_all_sitemap_links(self, concurrent_tasks=MAX_CONCURRENT_TASKS):
+    async def log(self, message):
+        await self.log_queue.put(message)
+
+    async def get_all_sitemap_links(self):
         async with aiohttp.ClientSession() as session:
             async with session.get(self.sitemaps_link, headers=USER_AGENT) as response:
                 if response.status != 200:
-                    self.log_container.markdown(
-                        f"Error fetching sitemap: {response.status}")
+                    await self.log(f"Error fetching sitemap: {response.status}")
                     return []
 
                 soup = BeautifulSoup(await response.text(), "xml")
                 sitemaps = [loc.text for loc in soup.find_all("loc")]
 
                 all_urls = []
-                semaphore = asyncio.Semaphore(concurrent_tasks)
+                semaphore = asyncio.Semaphore(self.max_concurrent_tasks)
                 tasks = [self.fetch_sitemap_links(
                     session, sitemap, semaphore) for sitemap in sitemaps]
 
@@ -49,18 +51,16 @@ class Site(object):
         async with semaphore:
             async with session.get(sitemap, headers=USER_AGENT) as response:
                 if response.status != 200:
-                    self.log_container.markdown(
-                        f"Error fetching sitemap: {response.status}")
+                    await self.log(f"Error fetching sitemap: {response.status}")
                     return []
 
                 soup = BeautifulSoup(await response.text(), "xml")
                 urls = [loc.text for loc in soup.find_all("loc")]
-                self.log_container.markdown(
-                    f"Found {len(urls)} links in sitemap: {sitemap}")
+                await self.log(f"Found {len(urls)} links in sitemap: {sitemap}")
                 return urls
 
     async def scrape_article(self, session, url):
-        self.log_container.markdown(f"Fetching article from {url}...")
+        await self.log(f"Fetching article from {url}...")
         async with session.get(url, headers=USER_AGENT) as response:
             soup = BeautifulSoup(await response.text(), "html.parser")
 
@@ -69,7 +69,7 @@ class Site(object):
             ) if soup.find("h1") else "No Title"
             tags = [tag.get_text()
                     for tag in soup.select(".article__tag .box-content a")]
-            self.log_container.markdown(f'tags: {tags}')
+            await self.log(f'tags: {tags}')
             datetime_tag = soup.find("cms-date")
             article_datetime = datetime_tag["content"] if datetime_tag else datetime.now(
             ).isoformat()
@@ -102,13 +102,11 @@ class Site(object):
         self.conn.commit()
 
     def save_to_db(self, data):
-        self.log_container.markdown(f"Saving article to database: {data['url']}")
         self.cursor.execute('''INSERT OR IGNORE INTO articles (datetime, site, url, title, content, tags)
                     VALUES (?, ?, ?, ?, ?, ?)''',
                             (data["datetime"], self.domain, data["url"], data["title"], data["content"], data["tags"]))
 
     def update_to_db(self, url, data):
-        self.log_container.markdown(f"Updating article in database: {url}")
         self.cursor.execute('''
             INSERT INTO articles (datetime, site, url, title, content, tags)
             VALUES (?, ?, ?, ?, ?, ?)
@@ -141,29 +139,29 @@ class Site(object):
         self.conn.close()
 
 
-async def fetch_urls(sites, log_container):
+async def fetch_urls(sites, log_queue):
     for item in sites:
-        site = Site(**item, log_container=log_container)
+        site = Site(**item)
         site.init_db()
-        log_container.markdown(f"Fetching root sitemap from {site.domain}...")
-        links = await site.get_all_sitemap_links(concurrent_tasks=MAX_CONCURRENT_SITEMAPS)
+        await log_queue.put(f"Fetching root sitemap from {site.domain}...")
+        links = await site.get_all_sitemap_links()
         site.save_all_urls(links)
 
 
-async def crawl_sites(sites, log_container):
+async def crawl_sites(sites, log_queue):
     for item in sites:
-        site = Site(**item, log_container=log_container)
+        site = Site(**item)
         site.init_db()
         async with aiohttp.ClientSession() as session:
             tasks = []
             links = site.get_all_urls()
             for link in links:
                 if site.url_content_exists(link):
-                    log_container.markdown(f"URL content already exists: {link}")
+                    await log_queue.put(f"URL content already exists: {link}")
                     continue
 
                 tasks.append(site.scrape_article(session, link))
-                if len(tasks) >= MAX_CONCURRENT_TASKS:
+                if len(tasks) >= site.max_concurrent_tasks:
                     await asyncio.gather(*tasks)
                     site.conn.commit()
                     tasks = []
@@ -173,8 +171,16 @@ async def crawl_sites(sites, log_container):
                 await asyncio.gather(*tasks)
                 site.conn.commit()
 
-        log_container.markdown(f"Crawling completed for {site.domain}.")
+        await log_queue.put(f"Crawling completed for {site.domain}.")
         site.done()
+
+
+async def log_writer(log_queue, log_container):
+    while True:
+        message = await log_queue.get()
+        if message is None:
+            break
+        log_container.write(message)
 
 
 def parse_info(domains, sitemaps, exclude_urls):
@@ -228,9 +234,15 @@ def count_total_rows(site_filter, url_filter, title_filter):
     return total_rows
 
 
+def run_asyncio_tasks(tasks):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(asyncio.gather(*tasks))
+    loop.close()
+
+
 def main():
     st.title('Website Crawling Tool')
-
     domains = st.text_area('Enter a list of domains (comma separated)',
                            value="nhandan.vn",
                            help="Enter the domain names for crawling, e.g., 'nhandan.vn,example.com'")
@@ -243,22 +255,41 @@ def main():
                                 value="https://nhandan.vn/sitemaps/categories.xml,https://nhandan.vn/sitemaps/topics.xml",
                                 help="Enter the exclude URLs for each domain.")
 
+    max_concurrent_tasks = st.number_input(
+        'Enter max concurrent tasks when fetching content', min_value=1, value=1)
+    max_concurrent_sitemaps = st.number_input(
+        'Enter max concurrent tasks when fetching sitemaps urls', min_value=1, value=5)
+
     tab = st.sidebar.radio("Navigation", ["Crawl", "View Data"])
 
     log_placeholder = st.empty()
-    log_container = log_placeholder.container()
 
     if tab == "Crawl":
-        if st.button('Get all urls of sites'):
-            sites_to_crawl = parse_info(domains, sitemaps, exclude_urls)
-            asyncio.run(fetch_urls(sites_to_crawl, log_container))
-            log_container.markdown("Done")
+        log_queue = asyncio.Queue()
+        with log_placeholder.container() as log_container:
+            if st.button('Get all urls of sites'):
+                sites_to_crawl = parse_info(domains, sitemaps, exclude_urls)
+                for site in sites_to_crawl:
+                    site['max_concurrent_tasks'] = max_concurrent_tasks
+                    site['max_concurrent_sitemaps'] = max_concurrent_sitemaps
+                run_asyncio_tasks([
+                    fetch_urls(sites_to_crawl, log_queue),
+                    log_writer(log_queue, log_container)
+                ])
+                log_queue.put_nowait(None)  # Signal the log writer to stop
 
-        if st.button('Start Crawling content'):
-            sites_to_crawl = parse_info(domains, sitemaps, exclude_urls)
-            log_container.markdown("Starting the crawl process... Please wait.")
-            asyncio.run(crawl_sites(sites_to_crawl, log_container))
-            log_container.markdown("Crawling process completed!")
+            if st.button('Start Crawling content'):
+                sites_to_crawl = parse_info(domains, sitemaps, exclude_urls)
+                for site in sites_to_crawl:
+                    site['max_concurrent_tasks'] = max_concurrent_tasks
+                    site['max_concurrent_sitemaps'] = max_concurrent_sitemaps
+                log_queue.put_nowait(
+                    "Starting the crawl process... Please wait.")
+                run_asyncio_tasks([
+                    crawl_sites(sites_to_crawl, log_queue),
+                    log_writer(log_queue, log_container)
+                ])
+                log_queue.put_nowait(None)  # Signal the log writer to stop
 
     elif tab == "View Data":
         st.header("View Data")
